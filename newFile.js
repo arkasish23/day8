c428fc239f95b74abdb7d35519bcb08833cf1a74
Arkasish Pal <arkasishpal@gmail.com>
2023-05-29 10:36:51 -0700

    updated index.html

---
+++ newFile.js
@@ -0,0 +1,14 @@
+for (let ; ;)
+    do {
+        index;
+    } while (condition);
+0;
+index < array.length;
+index++;
+{
+    const element = array[index];
+    export default 8086;
+    export const open = ['index.html'];
+    export const host = '0.0.0.0';
+    export const https = true;
+}
